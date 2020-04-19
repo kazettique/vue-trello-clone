8a1452236cd68091cf0ec14fc3e40f2f20bc694a
Woody <woody.chang@cloud-interactive.com>
2020-04-19 10:50:37 +0800

    finished course

--- src/components/ColumnTask.vue
+++ src/components/ColumnTask.vue
@@ -1,13 +1,9 @@
 <template>
-  <div>
-    <div
+  <AppDrop @drop="moveTaskOrColumn">
+    <AppDrag
       class="task"
-      draggable
-      @dragstart="pickupTask($event, taskIndex, columnIndex)"
+      :transferData="{ type: 'task', fromColumnIndex: columnIndex, fromTaskIndex: taskIndex }"
       @click="goToTask(task)"
-      @dragover.prevent
-      @dragenter.prevent
-      @drop.stop="moveTaskOrColumn($event, column.tasks, columnIndex, taskIndex)"
     >
       <span class="w-full flex-no-shrink font-bold">
         {{ task.name }}
@@ -15,14 +11,17 @@
       <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
         {{ task.description }}
       </p>
-    </div>
-  </div>
+    </AppDrag>
+  </AppDrop>
 </template>
 
 <script>
 import movingTasksAndColumnsMixin from '../mixins/movingTasksAndColumnsMixin'
+import AppDrag from './AppDrag'
+import AppDrop from './AppDrop'
 
 export default {
+  components: { AppDrag, AppDrop },
   mixins: [movingTasksAndColumnsMixin],
   props: {
     task: {
@@ -35,14 +34,6 @@ export default {
     }
   },
   methods: {
-    pickupTask (e, taskIndex, fromColumnIndex) {
-      e.dataTransfer.effectAllowed = 'move'
-      e.dataTransfer.dropEffect = 'move'
-
-      e.dataTransfer.setData('from-task-index', taskIndex)
-      e.dataTransfer.setData('from-column-index', fromColumnIndex)
-      e.dataTransfer.setData('type', 'task')
-    },
     goToTask (task) {
       this.$router.push({ name: 'task', params: { id: task.id } })
     }
